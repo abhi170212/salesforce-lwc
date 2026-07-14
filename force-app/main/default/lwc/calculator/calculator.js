74c8de740f237369b66fd9e6656a5d091dc0faba
Abhishek Kumar Singh <ram07abhishek@gmail.com>
2026-07-14 23:40:13 +0530

    Salesforce Calculator with the help of LWC added

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -1,3 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track} from 'lwc';
 
-export default class Calculator extends LightningElement {}
+export default class Calculator extends LightningElement {
+     @track firstNumber;
+     @track secondNumber;
+     @track result;
+     @track calArray=[];
+     @track isShowAllChecked=false;
+
+     handleChange(evt){
+          const fieldValue = evt.target.name;
+          if(fieldValue === 'fnum'){
+               this.firstNumber=evt.target.value;
+          }else if(fieldValue === 'snum'){
+               this.secondNumber = evt.target.value;
+          }
+     }
+     calculation(evt){
+          //-----------------------------------------------
+          this.firstNumber = parseInt(this.firstNumber);
+          this.secondNumber=Number(this.secondNumber);
+          //-------------------------------------------------
+          const fieldName=evt.target.name;
+          if(fieldName==='add'){
+               this.result = this.firstNumber+this.secondNumber;
+               this.calArray.push(this.result);
+          }else if(fieldName==='sub'){
+               this.result = this.firstNumber-this.secondNumber;
+               this.calArray.push(this.result);
+          }else if(fieldName === 'mul'){
+               this.result = this.firstNumber * this.secondNumber;
+               this.calArray.push(this.result);
+          }else if(fieldName==='div'){
+               this.result = this.firstNumber / this.secondNumber;
+               this.calArray.push(this.result);
+          }
+     }
+
+     showCalculation(evt){
+          if(evt.target.name==='all'){
+               this.isShowAllChecked=true;
+     }
+}
+}

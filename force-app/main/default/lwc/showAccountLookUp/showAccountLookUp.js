fb391fd780292c212ce167a6b0271f8549062996
Abhishek Kumar Singh <ram07abhishek@gmail.com>
2026-07-17 13:20:38 +0530

    lookup relationship established

--- force-app/main/default/lwc/showAccountLookUp/showAccountLookUp.js
+++ force-app/main/default/lwc/showAccountLookUp/showAccountLookUp.js
@@ -1,13 +1,46 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
+import callApex from '@salesforce/apex/LookUPHelper.getContacts';
+
+
 
 export default class ShowAccountLookUp extends LightningElement {
+     accountId;
+     contactList;
+     showContactTable=false;
+
+     columns=[{label:'Name',fieldName:'Name'},
+          {label:'Phone',fieldName:'Phone'}
+     ]
+     handleChange(evt){
+          this.accountId= evt.detail.recordId;
+          console.log(this.accountId); // debug ke liye
+          this.contactList=[];
+           this.showContactTable=true; 
+     }
+
+     @wire(callApex,{accId:'$accountId'})
+     wireData({error,data}){
+          if(data){
+               this.contactList=data;
+               this.showContactTable=true; 
+          }else if(error){
+               this.contactList=undefined;
+               this.showContactTable = false;
+               console.error(error);
+
+          }
+     }
+
+
+
+
 
      filter={
           criteria:[
                {
                     fieldPath:'CreatedDate',
                     operator:'eq',
-                    value:{literal:'TODAY'}
+                    value:{literal:'THIS WEEK'}
                }
           ]
      }
